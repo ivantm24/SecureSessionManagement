256cf5c6d7cf1c7948783aa35ee6bf345943b895
ivan tactuk mercado <ivantm24@gmail.com>
2015-11-13 10:30:50 -0500

    implemented login and logout

---
+++ src/java/ManageBean/MbSession.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManageBean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author ivantactukmercado
+ */
+@ManagedBean
+@RequestScoped
+public class MbSession {
+    
+    private String user;
+
+    private final HttpServletRequest httpServletRequest ;
+    private final FacesContext faceContext;
+    private FacesMessage facesMessage;
+    
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+    /**
+     * Creates a new instance of MbSession
+     */
+    public MbSession() {
+        faceContext=FacesContext.getCurrentInstance();        
+        httpServletRequest=(HttpServletRequest)faceContext.getExternalContext().getRequest();
+        if (httpServletRequest.getSession().getAttribute("sessionUser")!=null){
+            user=httpServletRequest.getSession().getAttribute("sessionUser").toString();
+        }
+    }
+    
+    public String closeSession(){
+        httpServletRequest.getSession().removeAttribute("sessionUser");
+        facesMessage=new FacesMessage(FacesMessage.SEVERITY_INFO,"SESSION CLOSED SUCCESSFULLY",null);
+        faceContext.addMessage(null, facesMessage);
+        return "index";
+    }
+    
+}
